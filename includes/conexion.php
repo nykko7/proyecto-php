533c72bc685c1d96157a9da1935f7470913af40e
Nicolas <n.perezbus@gmail.com>
2018-11-04 16:33:06 -0300

    Agregar BD

--- includes/conexion.php
+++ includes/conexion.php
@@ -1,9 +1,14 @@
 <?php
 //CONEXION: 
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'blog_master';
+//$host = 'localhost';
+//$user = 'root';
+//$password = '';
+//$database = 'blog_master';
+
+$host = 'us-cdbr-iron-east-01.cleardb.net';
+$user = 'b1ad82d24d5637';
+$password = '82d2de50';
+$database = 'heroku_ffbc9ee6a7e3046';
 $db = mysqli_connect($host, $user, $password, $database);
 
 mysqli_query($db, "SET NAMES 'utf8'");
